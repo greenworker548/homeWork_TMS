0ecef49e1d5e8f9860997cbf95637dbea6d175cb
greenworker548 <green_worker@rambler.ru>
2022-10-12 16:38:13 +0300

    add solution for homework_33 tasks

---
+++ homeWork_lesson33/script.js
@@ -0,0 +1,121 @@
+//  С ниже приведенным объектом решить следующие задачи:
+//     1. Создать строку из названий предметов написаных через запятую
+//     2. Посчитать общее количество студентов и учителей на всех предметах
+//     3. Получить среднее количество студентов на всех пердметах
+//     4. Создать массив из объектов предметов
+//     5. Получить массив из предметов и отсортировать по количеству преподавателей на факультете от большего к меньшему
+
+const subjects = {
+    mathematics: {
+      students: 200,
+      teachers: 6
+    },
+    biology: {
+      students: 120,
+      teachers: 6
+    },
+    chemistry: {
+      students: 100,
+      teachers: 3
+    },
+    geography: {
+      students: 60,
+      teachers: 2
+    }
+}
+
+
+// Task 1
+function getStringFromObject (object = {}) {
+    if (typeof object === 'object') {
+        let str = ''
+
+        for (let key in object) {
+            str += key
+        }
+
+        return str
+    } else {
+        console.log('Значение не является объектом')
+    }
+}
+
+console.log(getStringFromObject(subjects))
+
+
+// Task 2
+function getSumPeople (object = {}) {
+    if (typeof object === 'object') {
+        let sumStudents = 0,
+            sumTeacher = 0
+        
+        for (let key in object) {
+            let newObject = object[key]
+            sumStudents += newObject.students
+            sumTeacher += newObject.teachers
+        }
+
+        let sumResult = sumStudents + sumTeacher
+
+        return `Студентов ${sumStudents}, Учителей ${sumTeacher}, Всего ${sumResult}`
+    } else {
+        console.log('Значение не является объектом')
+    }
+}
+
+console.log(getSumPeople(subjects))
+
+
+// Task 3
+function getAverageStudents (object = {}) {
+    if (typeof object === 'object') {
+        let sumStudents = 0,
+            valueSubjects = 0
+        
+        for (let key in object) {
+            valueSubjects++
+            let newObject = object[key]
+            sumStudents += newObject.students
+        }
+
+        let averageResult = sumStudents / valueSubjects
+
+        return `Среднее значение студентов ${averageResult}`
+    } else {
+        console.log('Значение не является объектом')
+    }
+}
+
+console.log(getAverageStudents(subjects))
+
+
+// Task 4
+function transformObjectToArr (object = {}) {
+    if (typeof object === 'object') {
+        let newArr = []
+
+        for (let key in object) {
+            let newObject = object[key]
+            newObject.subjects = key
+            newArr.push(newObject)
+        }
+
+        return newArr
+    } else {
+        console.log('Значение не является объектом')
+    }
+}
+
+console.log(transformObjectToArr(subjects))
+
+
+// Task 5
+function sortArr (object = {}) {
+    let mutableArray = transformObjectToArr(object)
+
+    mutableArray.sort((a, b) => parseFloat(b.teachers) - parseFloat(a.teachers))
+
+    return mutableArray
+}
+
+console.log(sortArr(subjects))
