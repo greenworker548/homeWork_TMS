569af485ad9ca12d479fd88e65ac17c6c024fef6
greenworker548 <green_worker@rambler.ru>
2022-10-18 20:24:12 +0300

    add solution fron tasks home work 2

--- homeWork_2/index.ts
+++ homeWork_2/index.ts
@@ -1,199 +1,64 @@
 // Протипизировать данные
 // используйте все возможные типы для типизации, включая пользовательские типы
 
-// Array
-const faculties: (number | string | object)[] = [
-    {
-        id: 1,
-        faculty: "History department",
-        subjects: ["The World History", "History of Rome"],
-        countStudents: 44
-    },
-    {
-        id: 2,
-        faculty: "Department of Biology",
-        subjects: ["biology", "chemistry"],
-        countStudents: 50
-    },
-    {
-        id: 3,
-        faculty: "Faculty of Mathematics",
-        subjects: ["mathematics", "geometry", "trigonometry"],
-        countStudents: 72
-    },
-    {
-        id: 4,
-        faculty: "Faculty of Design",
-        subjects: ["ui", "ux", "graphic design"],
-        countStudents: 37
-    }
-]
-
-
 // Types
-type Faculties2 = [
-    {
-        id: number,
-        faculty: string,
-        subjects: string[],
-        countStudents: number
-    },
-    {
-        id: number,
-        faculty: string,
-        subjects: string[],
-        countStudents: number
-    },
-    {
-        id: number,
-        faculty: string,
-        subjects: string[],
-        countStudents: number
-    },
-    {
-        id: number,
-        faculty: string,
-        subjects: string[],
-        countStudents: number
-    }
-]
-
-const faculties2: Faculties2 = [
-    {
-        id: 1,
-        faculty: "History department",
-        subjects: ["The World History", "History of Rome"],
-        countStudents: 44
-    },
-    {
-        id: 2,
-        faculty: "Department of Biology",
-        subjects: ["biology", "chemistry"],
-        countStudents: 50
-    },
-    {
-        id: 3,
-        faculty: "Faculty of Mathematics",
-        subjects: ["mathematics", "geometry", "trigonometry"],
-        countStudents: 72
-    },
-    {
-        id: 4,
-        faculty: "Faculty of Design",
-        subjects: ["ui", "ux", "graphic design"],
-        countStudents: 37
-    }
-]
-
-
-// Interface (попытки протипизировать через интрефейсы, но я так понимаю при структуре данных как в задании - интерфейсы использовать не получится)
-interface ObjectArr {
+type Faculties = {
     id: number,
     faculty: string,
     subjects: string[],
     countStudents: number
 }
 
-interface Faculties3 {
-    1: ObjectArr[]
-}
-
-const faculties3: Faculties3 = {
-    1:[{
+const faculties2: Faculties[] = [
+    {
         id: 1,
         faculty: "History department",
         subjects: ["The World History", "History of Rome"],
         countStudents: 44
-    }]
-}
-
-
-// Array
-const movies: (number | string | object)[] = [
-    {
-        id: 1,
-        title: "Black Widow",
-        year: 2021,
-        released: "09 Jul 2021",
-        runtime: "134 min",
-        genre: ["Action", "Sci-Fi", "Adventure"],
-        director: "Cate Shortland",
-        writer: "Eric Pearson, Jac Schaeffer, Ned Benson",
-        actors: ["Scarlett Johansson", "Florence Pugh", "David Harbour"],
-        plot: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
-        country: "United States",
-        poster: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
-        imdbRating: 6.9,
-        imdbVotes: 121932,
-        type: "movie",
-        boxOffice: "$138,027,361",
-        production: "Marvel Studios",
     },
     {
         id: 2,
-        title: "Harry Potter and the Deathly Hallows: Part 2",
-        year: 2011,
-        released: "15 Jul 2011",
-        runtime: "130 min",
-        genre: ["Adventure", "Drama", "Fantasy"],
-        director: "David Yates",
-        writer: "Steve Kloves, J.K. Rowling",
-        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
-        plot: "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
-        country: "United Kingdom, United States",
-        poster: "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
-        imdbRating: 8.1,
-        imdbVotes: 790377,
-        type: "movie",
-        boxOffice: "$381,409,310",
-        production: "Heyday Films, Moving Picture Company, Warner Bros.",
+        faculty: "Department of Biology",
+        subjects: ["biology", "chemistry"],
+        countStudents: 50
     },
+    {
+        id: 3,
+        faculty: "Faculty of Mathematics",
+        subjects: ["mathematics", "geometry", "trigonometry"],
+        countStudents: 72
+    },
+    {
+        id: 4,
+        faculty: "Faculty of Design",
+        subjects: ["ui", "ux", "graphic design"],
+        countStudents: 37
+    }
 ]
 
 
 //Types
-type Movies2 = [
-    {
-        id: number,
-        title: string,
-        year: number,
-        released: string,
-        runtime: string,
-        genre: string[],
-        director: string,
-        writer: string,
-        actors: string[],
-        plot: string,
-        country: string,
-        poster: string,
-        imdbRating: number,
-        imdbVotes: number,
-        type: string,
-        boxOffice: string,
-        production: string
-    },
-    {
-        id: number,
-        title: string,
-        year: number,
-        released: string,
-        runtime: string,
-        genre: string[],
-        director: string,
-        writer: string,
-        actors: string[],
-        plot: string,
-        country: string,
-        poster: string,
-        imdbRating: number,
-        imdbVotes: number,
-        type: string,
-        boxOffice: string,
-        production: string
-    }
-]
+type Movies = {
+    id: number,
+    title: string,
+    year: number,
+    released: string,
+    runtime: string,
+    genre: string[],
+    director: string,
+    writer: string,
+    actors: string[],
+    plot: string,
+    country: string,
+    poster: string,
+    imdbRating: number,
+    imdbVotes: number,
+    type: string,
+    boxOffice: string,
+    production: string
+}
 
-const movies2: Movies2 = [
+const movies: Movies[] = [
     {
         id: 1,
         title: "Black Widow",
