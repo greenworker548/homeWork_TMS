b279f8692b443326be74f6f7a69aadf59dcf8140
greenworker548 <green_worker@rambler.ru>
2022-10-23 20:45:49 +0300

    add solution from hw_4

---
+++ homeWork_4/index.ts
@@ -0,0 +1,191 @@
+type Film = {
+    id?: number,
+    title: string,
+    year: number,
+    released: string,
+    runtime: string,
+    genre: string[],
+    director: string,
+    writer: string,
+    actors: string[],
+    plot: string,
+    country: string,
+    poster: string,
+    imdbRating: number,
+    imdbVotes: number,
+    type?: string,
+    boxOffice: string,
+    production: string
+}
+
+const films: Film[] = [
+    {
+        id: 1,
+        title: "Black Widow",
+        year: 2021,
+        released: "09 Jul 2021",
+        runtime: "134 min",
+        genre: ["Action", "Sci-Fi", "Adventure"],
+        director: "Cate Shortland",
+        writer: "Eric Pearson, Jac Schaeffer, Ned Benson",
+        actors: ["Scarlett Johansson", "Florence Pugh", "David Harbour"],
+        plot: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
+        country: "United States",
+        poster: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+        imdbRating: 6.9,
+        imdbVotes: 121932,
+        type: "movie",
+        boxOffice: "$138,027,361",
+        production: "Marvel Studios",
+    },
+    {
+        id: 2,
+        title: "Harry Potter and the Deathly Hallows: Part 2",
+        year: 2011,
+        released: "15 Jul 2011",
+        runtime: "130 min",
+        genre: ["Adventure", "Drama", "Fantasy"],
+        director: "David Yates",
+        writer: "Steve Kloves, J.K. Rowling",
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+        plot: "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+        country: "United Kingdom, United States",
+        poster: "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+        imdbRating: 8.1,
+        imdbVotes: 790377,
+        type: "movie",
+        boxOffice: "$381,409,310",
+        production: "Heyday Films, Moving Picture Company, Warner Bros.",
+    },
+    {
+        id: 3,
+        title: "Star Wars",
+        year: 1977,
+        released: "25 May 1977",
+        runtime: "121 min",
+        genre: ["Action", "Adventure", "Fantasy"],
+        director: "George Lucas",
+        writer: "George Lucas",
+        actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+        plot: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vad",
+        country: "United States, United Kingdom",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+        imdbRating: 8.6,
+        imdbVotes: 1259440,
+        type: "movie",
+        boxOffice: "$460,998,507",
+        production: "Lucasfilm Ltd.",
+    },
+    {
+        id: 4,
+        title: "Harry Potter and the Half-Blood Prince",
+        year: 2009,
+        released: "15 Jul 2009",
+        runtime: "153 min",
+        genre: ["Action", "Adventure", "Family"],
+        director: "David Yates",
+        writer: "Steve Kloves, J.K. Rowling",
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+        plot: "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort\'s dark past.",
+        country: "United Kingdom",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_SX300.jpg",
+        imdbRating: 7.6,
+        imdbVotes: 492245,
+        boxOffice: "$302,305,431",
+        production: "Heyday Films, Warner Bros.",
+    },
+    {   
+        title: "Harry Potter and the Sorcerer's Stone",
+        year: 2001,
+        released: "16 Nov 2001",
+        runtime: "152 min",
+        genre: ["Adventure", "Family", "Fantasy"],
+        director: "Chris Columbus",
+        writer: "J.K. Rowling, Steve Kloves",
+        actors: ["Daniel Radcliffe", "Rupert Grint", "Richard Harris"],
+        plot: "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
+        country: "United Kingdom, United States",
+        poster: "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+        imdbRating: 7.6,
+        imdbVotes: 684604,
+        boxOffice: "$318,087,620",
+        production: "1492 Pictures, Heyday Films, Warner Brothers",
+    }
+]
+
+
+// Task 1
+function getFilmsGenres (films:Film[]):string[] {
+    const arrayClean = films
+    .map((item:Film) => item.genre)
+    .reduce((acc:string[], item:string[]) => acc.concat(item),[])
+
+    const result = arrayClean.filter((item:string, index:number) => arrayClean.indexOf(item) === index)
+
+    return result
+}
+
+
+// Task 2
+function getFilmsActors (films:Film[]):string[] {
+    const arrayClean = films
+    .map((item:Film) => item.actors)
+    .reduce((acc:string[], item:string[]) => acc.concat(item),[])
+
+    const result = arrayClean.filter((item:string, index:number) => arrayClean.indexOf(item) === index)
+
+    return result
+}
+
+
+// Task 3
+function sortByRating (films:Film[]):Film[] {
+   return films.sort((a:Film, b:Film) => b.imdbRating - a.imdbRating)
+}
+
+
+// Task 4
+type FilmBriefDescription = {
+    id?: number,
+    title: string,
+    released: string,
+    plot: string
+}
+
+function getBriefDescription (films:Film[]):FilmBriefDescription[] {
+    return films.map((item:Film) => {
+        const result: FilmBriefDescription = {
+            id: item.id,
+            title: item.title,
+            released: item.released,
+            plot: item.plot
+        }
+
+        return result
+    })
+}
+
+
+// Task 5
+function searchByYear (films:Film[], year:number):Film[] {
+    return films.filter((item:Film) => item.year === year)
+}
+
+
+// Task 6, не хватило ума и времени сделать поиск по кускам слов(( будет работать только если ввести полное название
+function searchByTitle (films:Film[], title:string):Film[] {
+    return films.filter((item:Film) => {
+        // item.title.split(' ').filter((item) => item === title)
+        return item.title === title
+    })
+}
+
+
+// Task 7, тоже самое(
+function searchByTitleAndPlot (films:Film[], title:string) {}
+
+
+// Task 8
+function getFilmsId (films:Film[], key:string, value: number | string ) {
+    films.filter((item: any) => item[key] === value)
+}
